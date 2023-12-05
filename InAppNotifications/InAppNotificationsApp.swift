a41251f6e7abc4827e4eb649896d1ce36933d51f
christoseteoglou <itsmeglue@gmail.com>
2023-12-05 12:26:21 +0100

    Initial Commit

---
+++ InAppNotifications/InAppNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppNotificationsApp.swift
+//  InAppNotifications
+//
+//  Created by Christos Eteoglou on 2023-12-05.
+//
+
+import SwiftUI
+
+@main
+struct InAppNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
